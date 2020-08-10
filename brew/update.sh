09a07cdf09644dbf43a92acad8166577636cf30c
Ken Crandall <ken.crandall@gmail.com>
2020-08-10 14:14:59 -0700

    Update homebrew scripts

---
+++ brew/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#DIR=$HOME/Files
+DIR=$(pwd)
+
+echo "Checking homebrew pours..."
+echo "Brew List" > $DIR/brew-list.txt
+echo "---------" >> $DIR/brew-list.txt
+brew list >> $DIR/brew-list.txt
+
+echo "Checking homebrew casks..."
+echo "Brew Cask List" > $DIR/brew-cask-list.txt
+echo "--------------" >> $DIR/brew-cask-list.txt
+brew cask list >> $DIR/brew-cask-list.txt
+
+echo "Checking homebrew taps..."
+echo "Brew Tap" > $DIR/brew-tap.txt
+echo "--------" >> $DIR/brew-tap.txt
+brew tap >> $DIR/brew-tap.txt
+
+echo "Generating bundle (Brewfile)..."
+brew bundle dump -f
+
+echo "Done!"
