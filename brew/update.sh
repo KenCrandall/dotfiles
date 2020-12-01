46bc02251d85c3ff0988d6c81aee051bf8cecbd2
Ken Crandall <ken.crandall@gmail.com>
2020-11-30 19:55:22 -0800

    Updated homebrew scripts

--- brew/update.sh
+++ brew/update.sh
@@ -4,21 +4,22 @@
 DIR=$(pwd)
 
 echo "Checking homebrew pours..."
-echo "Brew List" > $DIR/brew-list.txt
-echo "---------" >> $DIR/brew-list.txt
-brew list >> $DIR/brew-list.txt
+echo "Brew List" >$DIR/brew-list.txt
+echo "---------" >>$DIR/brew-list.txt
+brew list --formula >>$DIR/brew-list.txt
 
 echo "Checking homebrew casks..."
-echo "Brew Cask List" > $DIR/brew-cask-list.txt
-echo "--------------" >> $DIR/brew-cask-list.txt
-brew list --cask >> $DIR/brew-cask-list.txt
+echo "Brew Cask List" >$DIR/brew-cask-list.txt
+echo "--------------" >>$DIR/brew-cask-list.txt
+brew list --cask >>$DIR/brew-cask-list.txt
 
 echo "Checking homebrew taps..."
-echo "Brew Tap" > $DIR/brew-tap.txt
-echo "--------" >> $DIR/brew-tap.txt
-brew tap >> $DIR/brew-tap.txt
+echo "Brew Tap" >$DIR/brew-tap.txt
+echo "--------" >>$DIR/brew-tap.txt
+brew tap >>$DIR/brew-tap.txt
 
 echo "Generating bundle (Brewfile)..."
-brew bundle dump -f
+HOST=$(hostname -s | tr '[:upper:]' '[:lower:]')
+brew bundle dump -f --file=Brewfile.${HOST}
 
 echo "Done!"
