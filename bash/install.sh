526a7d57ad6c1c5d6434a15274f3ba41a3e65d10
Ken Crandall <ken.crandall@gmail.com>
2020-12-03 13:47:57 -0800

    nabled backup option to install script

--- bash/install.sh
+++ bash/install.sh
@@ -5,6 +5,6 @@ echo "Installing BASH Prompt"
 for FILE in dot.*; do
   DEST=$(echo $FILE | cut -c 4-)
   echo "Copying $FILE to $HOME/$DEST"
-  install -C $FILE $HOME/$DEST
+  install -b -C $FILE $HOME/$DEST
 done
 
