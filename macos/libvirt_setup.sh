bceec07dc3e7ace724ae68d153057ef7567da75a
Ken Crandall <ken.crandall@gmail.com>
2021-07-28 17:47:14 -0700

    Add libvirt_setup script

---
+++ macos/libvirt_setup.sh
@@ -0,0 +1,3 @@
+echo 'security_driver = "none"' >> /usr/local/etc/libvirt/qemu.conf
+echo "dynamic_ownership = 0" >> /usr/local/etc/libvirt/qemu.conf
+echo "remember_owner = 0" >> /usr/local/etc/libvirt/qemu.conf
