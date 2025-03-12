d486e60d147cffb4c40ffa801d491d6c9bb38a00
Ken Crandall <ken@crandallnet.com>
2025-03-12 16:55:57 -0700

    homebrew updated at 2025-03-12 at 16:55:53 for mini

--- brew/update.sh
+++ brew/update.sh
@@ -4,41 +4,41 @@ TIME=$(date "+%Y-%m-%d at %H:%M:%S")
 MYOS=$(uname -s)
 OS=$(echo "$MYOS" | tr '[:upper:]' '[:lower:]')
 BREW=$(which brew)
-BASEDIR=$(pwd)
+BASEDIR="$(pwd)"
 HOST=$(hostname -s | tr '[:upper:]' '[:lower:]')
-DIR=$BASEDIR/$HOST
+DIR="${BASEDIR}/${HOST}"
 
 case $OS in
-  darwin | linux)
-    # macos or linux
-    if [[ -x $BREW ]]; then
-      echo "Executable 'brew' found at $BREW"
-    else
-      echo "Executable 'brew' not found"
-      exit
-    fi
-  ;;
-  *)
-    # no clue
-    echo "Unrecognized OS: No special sauce"
+darwin | linux)
+  # macos or linux
+  if [[ -x $BREW ]]; then
+    echo "Executable 'brew' found at $BREW"
+  else
+    echo "Executable 'brew' not found"
     exit
-	;;
+  fi
+  ;;
+*)
+  # no clue
+  echo "Unrecognized OS: No special sauce"
+  exit
+  ;;
 esac
 
-mkdir -p $DIR
-BLIST=$DIR/brew-list-${HOST}.txt
+mkdir -p "${DIR}"
+BLIST="${DIR}/brew-list-${HOST}.txt"
 echo "Checking homebrew pours..."
 echo "Brew List" >$BLIST
 echo "---------" >>$BLIST
 brew list --formula >>$BLIST
 
-BCASK=$DIR/brew-cask-list-${HOST}.txt
+BCASK="${DIR}/brew-cask-list-${HOST}.txt"
 echo "Checking homebrew casks..."
 echo "Brew Cask List" >$BCASK
 echo "--------------" >>$BCASK
 brew list --cask >>$BCASK
 
-BTAPS=$DIR/brew-tap-${HOST}.txt
+BTAPS="${DIR}/brew-tap-${HOST}.txt"
 echo "Checking homebrew taps..."
 echo "Brew Tap" >$BTAPS
 echo "--------" >>$BTAPS
